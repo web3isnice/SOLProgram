a5540a0b2c77328ac93e019d033fe9817c652f44
Web3isnice <121817789+web3isnice@users.noreply.github.com>
2025-02-22 02:29:38 +0100

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Token, TokenAccount, Transfer};
 
-declare_id!("6UhPPSPTANEjgLzgVr4Y4mUJSvMUJReUtghUHDQDyCKC");
+declare_id!("thiswillupdate");
 
 #[program]
 pub mod klend_liquidator {
